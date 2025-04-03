cb8cc84c79939baabac3a0ed61236e4108e22598
Thuan Nguyen <thuannv06022001@gmail.com>
2025-04-03 11:29:42 +0700

    update code

---
+++ src/components/ProductTable.js
@@ -0,0 +1,242 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import img from "../../src/asset/img/zed.png";
+
+const ProductTable = ({
+  products,
+  expandedRows,
+  handleRowClick,
+  handleSelectTab,
+  getActiveTab,
+}) => {
+  return (
+    <div>
+      <table className="table-product">
+        <thead>
+          <tr>
+            <th>
+              <input type="checkbox" />
+              <input type="checkbox" />
+            </th>
+            <th>
+              <span>Hình ảnh</span>
+            </th>
+            <th>
+              <span>Mã hàng</span>
+            </th>
+            <th>
+              <span>Tên hàng</span>
+            </th>
+            <th>
+              <span>Giá bán</span>
+            </th>
+            <th>
+              <span>Giá vốn</span>
+            </th>
+            <th>
+              <span>Thương hiệu</span>
+            </th>
+            <th>
+              <span>Tồn kho</span>
+            </th>
+            <th>
+              <span>Khách đặt</span>
+            </th>
+            <th>
+              <span>Thời gian tạo</span>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {products.map((item) => (
+            <React.Fragment key={item.productCode}>
+              <tr
+                className="products"
+                onClick={() => handleRowClick(item.productCode)}
+              >
+                <td>
+                  <input type="checkbox" name="" id="" />
+                  <input type="checkbox" />
+                </td>
+                <td>
+                  <img src={img} alt="" width="70" height="30" />
+                </td>
+                <td className="img-td">
+                  <span>SP00025</span>
+                </td>
+                <td>Bộ Lego City town square</td>
+                <td>3,199,000 </td>
+                <td>3,080,000</td>
+                <td>Alex Store </td>
+                <td>184</td>
+                <td>0</td>
+                <td>20/03/2025 8:30</td>
+              </tr>
+              {expandedRows.includes(item.productCode) && (
+                <tr className="product-detail-infor">
+                  <td colSpan="11">
+                    <div className="more-infor-product">
+                      <div className="tab-status-product">
+                        <span
+                          className={`${
+                            getActiveTab(item.productCode) === "inforProduct"
+                              ? "active"
+                              : ""
+                          }`}
+                          onClick={() =>
+                            handleSelectTab(item.productCode, "inforProduct")
+                          }
+                        >
+                          Thông tin
+                        </span>
+                        <span
+                          className={`${
+                            getActiveTab(item.productCode) === "storageTag"
+                              ? "active"
+                              : ""
+                          }`}
+                          onClick={() =>
+                            handleSelectTab(item.productCode, "storageTag")
+                          }
+                        >
+                          Thẻ kho
+                        </span>
+                        <span
+                          className={`${
+                            getActiveTab(item.productCode) === "currentStock"
+                              ? "active"
+                              : ""
+                          }`}
+                          onClick={() =>
+                            handleSelectTab(item.productCode, "currentStock")
+                          }
+                        >
+                          Tồn kho
+                        </span>
+                      </div>
+                      {getActiveTab(item.productCode) === "inforProduct" ? (
+                        <div className="product-detail">
+                          <div className="name-product">{item.productName}</div>
+                          <div className="status-product">
+                            <span>
+                              <i className="fa-solid fa-check"></i> Bán trực
+                              tiếp
+                            </span>
+                            <span>
+                              <i className="fa-solid fa-xmark"></i> Không tích
+                              điểm
+                            </span>
+                          </div>
+                          <div className="box-flex-product row">
+                            <div className="img-product col-lg-4 col-sm-6 d-flex justify-content-between">
+                              <img src={img} alt="" className="col-9" />
+                              <div className="thumbnail-img col-2">
+                                <img
+                                  src={img}
+                                  alt=""
+                                  width="100%"
+                                  className=""
+                                />
+                              </div>
+                            </div>
+                            <div className="infor-left col-lg-4 col-sm-6  ">
+                              <div className="form-group">
+                                <label htmlFor="">Mã hàng</label>
+                                <strong className="">{item.productCode}</strong>
+                              </div>
+                              <div className="form-group">
+                                <label htmlFor="">Mã vạch</label>
+                                <span className="">{item.barcode}</span>
+                              </div>
+                              <div className="form-group">
+                                <label htmlFor="">Loại hàng</label>
+                                <span className="">{item.productType}</span>
+                              </div>
+                              <div className="form-group">
+                                <label htmlFor="">Thương hiệu</label>
+                                <span className="">{item.brand}</span>
+                              </div>
+                              <div className="form-group">
+                                <label htmlFor="">Định mức tồn</label>
+                                <span className="">{item.stockLimit}</span>
+                              </div>
+                              <div className="form-group">
+                                <label htmlFor="">Giá bán</label>
+                                <span className="">{item.salePrice}</span>
+                              </div>
+                              <div className="form-group">
+                                <label htmlFor="">Giá vốn</label>
+                                <span className="">{item.costPrice}</span>
+                              </div>
+                              <div className="form-group">
+                                <label htmlFor="">Trọng lượng</label>
+                                <span className="">{item.weight}</span>
+                              </div>
+                              <div className="form-group">
+                                <label htmlFor="">Vị trí</label>
+                                <span className="">{item.location}</span>
+                              </div>
+                            </div>
+                            <div className="infor-right col-lg-4 col-sm-6 ">
+                              <div className="form-group">
+                                <label htmlFor="">Mô tả</label>
+                                <div className="form-control-static">
+                                  {item.description}
+                                </div>
+                              </div>
+                              <div className="form-group">
+                                <label htmlFor="">Ghi chú đặt hàng</label>
+                                <div className="form-control-static">
+                                  {item.orderNote}
+                                </div>
+                              </div>
+                              <div className="form-group">
+                                <label htmlFor="">Nhà cung cấp</label>
+                                <div className="form-control-static">
+                                  {item.supplier}
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                          <div className="group-btn">
+                            <Link className="btn btn-success">
+                              <i className="fa fa-check-square"></i>
+                              Cập nhật
+                            </Link>
+                            <Link className="btn btn-secondary">
+                              <i className="fa fa-barcode"></i>
+                              In tem mã
+                            </Link>
+                            <Link className="btn btn-success">
+                              <i className="far fa-clone"></i> Sao chép
+                            </Link>
+                            <Link className="btn btn-danger">
+                              <i className="fa fa-lock"></i> Ngừng kinh doanh
+                            </Link>
+                            <Link className="btn btn-danger">
+                              <i className="far fa-trash-alt"></i> Xóa
+                            </Link>
+                            <Link className="btn btn-primary">
+                              <i className="far fa-plus"></i> Thêm hàng hóa cùng
+                              loại
+                            </Link>
+                          </div>
+                        </div>
+                      ) : getActiveTab(item.productCode) === "storageTag" ? (
+                        <div className="product-detail ">thẻ kho</div>
+                      ) : (
+                        <div className="product-detail">tồn kho</div>
+                      )}
+                    </div>
+                  </td>
+                </tr>
+              )}
+            </React.Fragment>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default ProductTable;
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -4,12 +4,30 @@ import "../asset/style/product-page.scss";
 import { Link } from "react-router-dom";
 import ModalComponent from "../components/ModalComponent";
 import AddGoods from "../components/AddGoods";
-import img from "../../src/asset/img/zed.png";
+import ProductTable from "../components/ProductTable";
+import AddService from "../components/AddService";
+import AddCombo from "../components/AddCombo";
 const Products = () => {
   const dataProduct = [
     {
       productName: "Spririt Blossom Yone",
       productCode: "SP000025",
+      barcode: 8934626012203,
+      productCategory: "Thực phẩm ăn liền",
+      productType: "Hàng hóa",
+      brand: "mlemmlem",
+      stockLimit: "0 > 10",
+      salePrice: "10,000",
+      costPrice: "8,000",
+      weight: "10g",
+      location: "Việt Nam",
+      description: "Sản phẩm này đẹp quá",
+      orderNote: "Hàng dễ vỡ",
+      supplier: "CVT",
+    },
+    {
+      productName: "Spririt Blossom Yone",
+      productCode: "SP000023",
       barcode: 8934626012203,
       productCategory: "Thực phẩm ăn liền",
       productType: "Hàng hóa",
@@ -43,14 +61,15 @@ const Products = () => {
   });
   const [expandedRows, setExpandedRows] = useState([]);
   const [isModel, setIsModal] = useState(null);
+  const [isTab, setTab] = useState("inforProduct");
   const handleModel = (modal) => {
     setIsModal(
       modal === "goods"
         ? { content: <AddGoods /> }
         : modal === "service"
-        ? { content: "service" }
+        ? { content: <AddService /> }
         : modal === "combo"
-        ? { content: "combo" }
+        ? { content: <AddCombo /> }
         : null
     );
     console.log(modal);
@@ -68,6 +87,14 @@ const Products = () => {
         : [...prev, productCode]
     );
   };
+  const handleSelectTab = (productCOde, tab) => {
+    setTab((prevTab) => ({
+      ...prevTab,
+      [productCOde]: tab,
+    }));
+    console.log(tab);
+  };
+  const getActiveTab = (productCode) => isTab[productCode] || "inforProduct";
   return (
     <div className="product-view">
       <div className="section-left">
@@ -130,7 +157,7 @@ const Products = () => {
           </div>
           <div className="filter-products">
             <div className="btn-filter">
-              <span className="filter-title">
+              <span className="filter-title btn btn-success">
                 <i className="fa-solid fa-plus"></i> <span>Thêm mới</span>
                 <i className="fas fa-caret-down"></i>
               </span>
@@ -156,197 +183,32 @@ const Products = () => {
               </ul>
             </div>
             <div className="btn-filter" onClick={() => handleModel("import")}>
-              <span>
+              <span className="btn btn-success">
                 <i className="fas fa-file-import"></i>
                 <span>Nhập file</span>
               </span>
             </div>
             <div className="btn-filter" onClick={() => handleModel("export")}>
-              <span>
+              <span className="btn btn-success">
                 <i className="fas fa-file-export"></i>
                 <span>Xuất file</span>
               </span>
             </div>
             <div className="btn-filter">
-              <span>
+              <span className="btn btn-success">
                 <i className="fas fa-bars"></i>
                 <i className="fas fa-caret-down"></i>
               </span>
             </div>
           </div>
         </div>
-        <table className="table-product">
-          <thead>
-            <tr>
-              <th>
-                <input type="checkbox" />
-                <input type="checkbox" />
-              </th>
-              <th>
-                <span>Mã hàng</span>
-              </th>
-              <th>
-                <span>Ten hàng</span>
-              </th>
-              <th>
-                <span>Giá bán</span>
-              </th>
-              <th>
-                <span>Giá vốn</span>
-              </th>
-              <th>
-                <span>Thương hiệu</span>
-              </th>
-              <th>
-                <span>Tồn kho</span>
-              </th>
-              <th>
-                <span>Khách đặt</span>
-              </th>
-              <th>
-                <span>Thời gian tạo</span>
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {dataProduct.map((item) => (
-              <React.Fragment key={item.productCode}>
-                <tr
-                  className="products"
-                  onClick={() => handleRowClick(item.productCode)}
-                >
-                  <td>
-                    <input type="checkbox" name="" id="" />
-                    <input type="checkbox" />
-                  </td>
-                  <td className="img-td">
-                    <img src={img} alt="" width={50} height={30} />
-                    <span>SP00025</span>
-                  </td>
-                  <td>Bộ Lego City town square</td>
-                  <td>3,199,000 </td>
-                  <td>3,080,000</td>
-                  <td>Alex Store </td>
-                  <td>184</td>
-                  <td>0</td>
-                  <td>20/03/2025 8:30</td>
-                </tr>
-                {expandedRows.includes(item.productCode) && (
-                  <tr className="product-detail-infor">
-                    <td colSpan="11">
-                      <div className="more-infor-product">
-                        <div className="tab-status-product">
-                          <span>Thông tin</span>
-                          <span>Thẻ kho</span>
-                          <span>Tồn kho</span>
-                        </div>
-                        <div className="product-detail">
-                          <div className="name-product">{item.productName}</div>
-                          <div className="status-product">
-                            <span>Bán trực tiếp</span>
-                            <span>Không tích điểm</span>
-                          </div>
-                          <div className="box-flex-product row">
-                            <div className="img-product col-lg-4 col-sm-6 d-flex justify-content-between">
-                              <img src={img} alt="" className="col-9" />
-                              <div className="thumbnail-img col-2">
-                                <img
-                                  src={img}
-                                  alt=""
-                                  width="100%"
-                                  className=""
-                                />
-                              </div>
-                            </div>
-                            <div className="infor-left col-lg-4 col-sm-6  ">
-                              <div className="form-group">
-                                <label htmlFor="">Mã hàng</label>
-                                <strong className="">{item.productCode}</strong>
-                              </div>
-                              <div className="form-group">
-                                <label htmlFor="">Mã vạch</label>
-                                <span className="">{item.barcode}</span>
-                              </div>
-                              <div className="form-group">
-                                <label htmlFor="">Loại hàng</label>
-                                <span className="">{item.productType}</span>
-                              </div>
-                              <div className="form-group">
-                                <label htmlFor="">Thương hiệu</label>
-                                <span className="">{item.brand}</span>
-                              </div>
-                              <div className="form-group">
-                                <label htmlFor="">Định mức tồn</label>
-                                <span className="">{item.stockLimit}</span>
-                              </div>
-                              <div className="form-group">
-                                <label htmlFor="">Giá bán</label>
-                                <span className="">{item.salePrice}</span>
-                              </div>
-                              <div className="form-group">
-                                <label htmlFor="">Giá vốn</label>
-                                <span className="">{item.costPrice}</span>
-                              </div>
-                              <div className="form-group">
-                                <label htmlFor="">Trọng lượng</label>
-                                <span className="">{item.weight}</span>
-                              </div>
-                              <div className="form-group">
-                                <label htmlFor="">Vị trí</label>
-                                <span className="">{item.location}</span>
-                              </div>
-                            </div>
-                            <div className="infor-right col-lg-4 col-sm-6 ">
-                              <div className="form-group">
-                                <label htmlFor="">Mô tả</label>
-                                <div className="form-control-static">
-                                  {item.description}
-                                </div>
-                              </div>
-                              <div className="form-group">
-                                <label htmlFor="">Mô tả</label>
-                                <div className="form-control-static">
-                                  {item.orderNote}
-                                </div>
-                              </div>
-                              <div className="form-group">
-                                <label htmlFor="">Mô tả</label>
-                                <div className="">{item.supplier}</div>
-                              </div>
-                            </div>
-                          </div>
-                          <div className="group-btn">
-                            <Link className="btn btn-success">
-                              <i className="fa fa-check-square"></i>
-                              Cập nhật
-                            </Link>
-                            <Link className="btn btn-secondary">
-                              <i className="fa fa-barcode"></i>
-                              In tem mã
-                            </Link>
-                            <Link className="btn btn-success">
-                              <i className="far fa-clone"></i> Sao chép
-                            </Link>
-                            <Link className="btn btn-danger">
-                              <i className="fa fa-lock"></i> Ngừng kinh doanh
-                            </Link>
-                            <Link className="btn btn-danger">
-                              <i className="far fa-trash-alt"></i> Xóa
-                            </Link>
-                            <Link className="btn btn-primary">
-                              <i className="far fa-plus"></i> Thêm hàng hóa cùng
-                              loại
-                            </Link>
-                          </div>
-                        </div>
-                      </div>
-                    </td>
-                  </tr>
-                )}
-              </React.Fragment>
-            ))}
-          </tbody>
-        </table>
+        <ProductTable
+          products={dataProduct}
+          expandedRows={expandedRows}
+          handleRowClick={handleRowClick}
+          handleSelectTab={handleSelectTab}
+          getActiveTab={getActiveTab}
+        ></ProductTable>
       </div>
 
       <ModalComponent isOpen={isModel} onClose={() => setIsModal(null)}>
